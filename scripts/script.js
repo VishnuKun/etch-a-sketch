9a540f7244e985c5e10f69e13b452bac4c27af6b
Vishnu <vishnukun954AV@protonmail.com>
2023-04-21 10:39:40 +0530

    Declared and created function createGrid to split grid container and insert new divs into it plus accessed grid container.

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+//accessing the grid container
+let gridBox = document.querySelector("#grid");
+//function to create grid as per the size
+function createGrid(size=16) {
+    //split container into columns and rows
+    gridBox.style.display = "grid";
+    gridBox.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    gridBox.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    //add new square divs
+    for (let i = 0; i < size**2; i++) {
+        let div = document.createElement("div");
+        div.className = "square";
+        div.id = "square"+i;
+        gridBox.insertAdjacentElement("beforeend", div);
+    }
+
+
+}
+
+createGrid();
